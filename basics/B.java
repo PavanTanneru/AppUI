906b1bd0785510907f8d9f19e855982b06721e8d
PavanTanneru <pavankumar.raju3656@gmail.com>
2021-10-19 11:01:01 +0530

    Adding the java basics programs to develop Branch

---
+++ basics/B.java
@@ -0,0 +1,11 @@
+class B
+{
+   	
+	public static void m1()
+	{
+	System.out.println("m1 starts to execute");
+	System.out.println("m1 stops to execute");
+	}
+}
+
+	
---
+++ basics/a.java
@@ -0,0 +1,8 @@
+class A
+{
+public static void main(String[] args)
+{
+System.out.println("hello pavan");
+System.out.println("you are started learning java");
+}
+}
